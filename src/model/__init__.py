3fb3ffeb49ad0ec6140de54394270d2e01c4a902
Thái Quang Phát <quangphatthai@gmail.com>
2025-10-23 11:31:44 +0700

    update: added new architecture

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,5 +1,7 @@
 from .model_1 import QueryPathRLV1
+from .model_2 import HierarchicalQueryPathRLV1
 
 QUERY_PATH_RL = {
-    "QueryPathRLV1": QueryPathRLV1
+    "QueryPathRLV1": QueryPathRLV1,
+    "QueryPathRLV2": HierarchicalQueryPathRLV1
 }
