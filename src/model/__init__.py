7ddc5563cc65c5b65930ba6f1d5493e97bf60cb4
Thái Quang Phát <quangphatthai@gmail.com>
2025-12-25 10:32:22 +0700

    model: added PPO model with training script

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,5 +1,6 @@
 from .model_0 import QueryPathRLV0
 from .model_0_1 import QueryPathActorCritic
+from .model_0_2 import QueryPathRLPPO
 from .model_1 import QueryPathRLV1
 from .model_2 import HierarchicalQueryPathRLV1
 from .model_3 import HierarchicalQueryPathRLV2
@@ -7,6 +8,7 @@ from .model_3 import HierarchicalQueryPathRLV2
 QUERY_PATH_RL = {
     "QueryPathRLV0": QueryPathRLV0,
     "QueryPathRLV01": QueryPathActorCritic,
+    "QueryPathRLV02": QueryPathRLPPO,
     "QueryPathRLV1": QueryPathRLV1,
     "QueryPathRLV2": HierarchicalQueryPathRLV1,
     "QueryPathRLV3": HierarchicalQueryPathRLV2
